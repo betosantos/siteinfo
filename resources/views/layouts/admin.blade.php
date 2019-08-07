a2111e6a2112100d39ee652e99bb7901ec1baf5f
betosantos <oliveira.roberto@gmail.com>
2019-08-07 13:02:31 -0300

    SiteInfo ajustadas lista de posts com imagem do banco de dados e imagem padrao

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -125,7 +125,7 @@
           <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
             <img src="{{ asset('admin/production/images/img.jpg') }}" alt="">
             @if (Auth::check())
-              {{ Auth::user()->name }}
+            {{ Auth::user()->name }}
             @endif
             <span class=" fa fa-angle-down"></span>
           </a>
@@ -208,10 +208,12 @@
 </div>
 <!-- /top navigation -->
 
+
+
 <!-- page content -->
 <div class="right_col" role="main">
   <!-- top tiles -->
-  <div class="row tile_count">
+  <!-- <div class="row tile_count">
     <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
       <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
       <div class="count">2500</div>
@@ -242,45 +244,17 @@
       <div class="count">7,325</div>
       <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
     </div>
-  </div>
+  </div> -->
   <!-- /top tiles -->
 
   <div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-
-    </div>
-
-  </div>
-  <br />
-
-
-
-  @yield('content')
-
-
-  <div class="row">
-    <div class="col-md-4 col-sm-4 col-xs-12">
-
-    </div>
-
-    <div class="col-md-8 col-sm-8 col-xs-12">
-      <div class="row">
-      </div>
-      <div class="row">
-
-
-        <!-- Start to do list -->
-        <div class="col-md-6 col-sm-6 col-xs-12">
-
-        </div>
-        <!-- End to do list -->
-
-
-      </div>
+    <div class="col-md-12 col-sm-4 col-xs-12"><!-- Painel em branco tamanho 12 -->
+      @yield('content')
     </div>
   </div>
 </div>
 <!-- /page content -->
+
 
 <!-- footer content -->
 <footer>
--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -12,35 +12,42 @@
 </div> -->
 
 <div class="col-md-12 col-sm-4 col-xs-12">
-  <div class="x_panel tile fixed_height_320">
-    <h3>Posts Cadastrados</h3>
+  <h3>Posts Cadastrados</h3>
 
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">Título</th>
+        <th scope="col">Descrição</th>
+        <!-- <th scope="col">Imagem</th> -->
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
+    @foreach($posts as $post)
+    <tbody>
+      <tr>
+        <!-- <th scope="row">1</th> -->
+        <td>
+          @if ($post['imagem'])
+          <img src="{{ asset('uploads/posts/'.$post->imagem) }}" width="80" height="80">
+          @else
+          <img src="{{ asset('uploads/posts/padrao.jpeg') }}" width="80" height="80">
+          @endif
+        </td>
+        <!-- <img src={{ asset('uploads/posts/'.$post->imagem) }}></td> -->
+        <td>{{ $post->titulo }}</td>
+        <td>{{ $post->descricao }}</td>
+        <td class="float-right">
+          <a href="#"><i class="fa fa-id-card-o" aria-hidden="true"></i></a> &nbsp;
+          <a href="#"><i class="fa fa-id-card-o" aria-hidden="true"></i></a>
+        </td>
+      </tr>
+      @endforeach
 
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Título</th>
-          <th scope="col">Descrição</th>
-          <!-- <th scope="col">Imagem</th> -->
-          <th scope="col">Ações</th>
-        </tr>
-      </thead>
-      @foreach($posts as $post)
-      <tbody>
-        <tr>
-          <!-- <th scope="row">1</th> -->
-          <td>{{ $post->titulo }}</td>
-          <td>{{ $post->descricao }}</td>
-          <td>
-            <a href="#"><i class="fa fa-id-card-o" aria-hidden="true"></i></a> &nbsp;
-            <a href="#"><i class="fa fa-id-card-o" aria-hidden="true"></i></a>
-          </td>
-        </tr>
-        @endforeach
+    </tbody>
+  </table>
 
-      </tbody>
-    </table>
-  </div>
 </div>
 
 
